fd2e38fc9bbfe8f32d8f3d6607719d3f64b08c55
Lucas <will.alan.smith@gmail.com>
2024-09-25 13:04:20 +0100

    Add theme context for light and dark mode

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,19 +2,29 @@ import './App.module.css'
 import StartMenu from '../StartMenu/StartMenu.jsx'
 import Quiz from '../Quiz/Quiz.jsx'
 import { useState } from 'react'
+import { ThemeContext } from '../../contexts/themeContext.jsx'
 
 function App() {
   const [quizSubject, setQuizSubject] = useState('')
+  const [theme, setTheme] = useState('light')
+
+  function toggleTheme() {
+    theme === 'light' ? setTheme('dark') : setTheme('light') 
+  }
 
   return (
-    quizSubject ? 
-    <Quiz 
-      subject={quizSubject} 
-      setQuizSubject={setQuizSubject} 
-    /> : 
-    <StartMenu 
-      setQuizSubject={setQuizSubject}
-    />
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      {
+        quizSubject ? 
+        <Quiz 
+          subject={quizSubject} 
+          setQuizSubject={setQuizSubject} 
+        /> : 
+        <StartMenu 
+          setQuizSubject={setQuizSubject}
+        />
+      }
+    </ThemeContext.Provider>
   )
 }
 
--- src/components/primitives/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/primitives/ToggleSwitch/ToggleSwitch.jsx
@@ -1,12 +1,33 @@
-import {toggleSwitch} from './ToggleSwitch.module.css'
+import {toggleSwitch, toggleContainer} from './ToggleSwitch.module.css'
+import darkModeSun from '/images/icon-sun-light.svg'
+import darkModeMoon from '/images/icon-moon-light.svg'
+import lightModeSun from '/images/icon-sun-dark.svg'
+import lightModeMoon from '/images/icon-moon-dark.svg'
+import { useContext } from 'react'
+import { ThemeContext } from '../../../contexts/themeContext'
 
 function ToggleSwitch({className}) {
+    const {theme, toggleTheme} = useContext(ThemeContext)
+
+    let sunImg = lightModeSun
+    let moonImg = lightModeMoon
+
+    if (theme === 'dark') {
+        sunImg = darkModeSun
+        moonImg = darkModeMoon
+    }
+
     return (
-        <input 
-            className={toggleSwitch + " " + className} 
-            type="checkbox" 
-            role="switch" 
-        />
+        <div className={toggleContainer + " " + className}>
+            <img src={sunImg} alt="light setting"/>
+            <input 
+                className={toggleSwitch} 
+                type="checkbox" 
+                role="switch" 
+                onClick={toggleTheme}
+            />
+            <img src={moonImg} alt="dark setting"/>
+        </div>
     )
 }
 
